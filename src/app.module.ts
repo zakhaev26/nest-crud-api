a75f69774e80d324ab0e9b3744bdc5f464f28bef
= <=>
2023-10-29 16:46:36 +0530

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AlbumsController } from './albums.controller';
 import { DynamicController } from './dynamic.controller';
 import { PostExtractor } from './extract.post.controller';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
 @Module({
   imports: [],
   controllers:[UserController,AlbumsController,DynamicController,PostExtractor],
-  providers:[]
+  providers:[UserService]
 })
+
 export class AppModule {}
--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,18 +1,82 @@
+// /**
+//  * 
+// * Handling all user related Fucntions in Memory
+
+// **/
+
+// import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+// import { UserDataType } from "./types/index";
+
+// let USERS = [];
+
+// @Controller('/v1users')
+// export class UserController {
+
+
+
+
+//     @Post('/createUser')
+//     addUser(@Body() newUser : UserDataType) {
+//         USERS.push(newUser);
+//         return "Success!";
+//     }
+
+//     @Get('/alluser')
+//     getAllUser() {
+//         return USERS;
+//     }
+
+//     @Put('/updateuser/:id')
+//     updateUser(@Param('id') id: number,@Body() updatedUser : UserDataType) {
+//         const idx = USERS.findIndex(user=>{
+//             user.id ===id;
+//         })
+
+//         if(!idx) return;
+//         USERS[idx] = updatedUser;
+//         return "Successfully Updated!";
+//     }
+
+//     @Get('/specific/user/:id')
+//     getSpecific(@Param('id') id : number) {
+//         const theUser = USERS.find(user=>user.id === id)
+        
+//         if(!theUser) return "noUser";
+//         return theUser;
+//     }
+  
+//     @Delete('/delete/:id')
+//     deleteUser(@Param('id') id : number ) {
+//         USERS = USERS.filter(user=>{
+//             user.id!==id;
+//         })
+//         return "Deleted!"
+//     }
+// }
+
+
+
 /**
- * Handling all user related Fucntions in Memory
- */
+ * 
+* Handling all user related Fucntions in Memory
+
+**/
 
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { UserDataType } from "./types/index";
+import { UserService } from "./user.service";
 
 let USERS = [];
 
 @Controller('/v1users')
 export class UserController {
 
+    constructor(private usersService : UserService) {}
+
     @Post('/createUser')
     addUser(@Body() newUser : UserDataType) {
-        USERS.push(newUser);
+
+        this.usersService.addUser(newUser);
         return "Success!";
     }
 
@@ -23,26 +87,17 @@ export class UserController {
 
     @Put('/updateuser/:id')
     updateUser(@Param('id') id: number,@Body() updatedUser : UserDataType) {
-        const idx = USERS.findIndex(user=>{
-            user.id ===id;
-        })
-
-        if(!idx) return;
-        USERS[idx] = updatedUser;
-        return "Successfully Updated!";
+        return this.usersService.updateUser(id,updatedUser);
     }
+
     @Get('/specific/user/:id')
     getSpecific(@Param('id') id : number) {
-        const theUser = USERS.find(user=>user.id === id)
-        
-        if(!theUser) return "noUser";
-        return theUser;
+        return this.usersService.getUser(id);
     }
+  
     @Delete('/delete/:id')
     deleteUser(@Param('id') id : number ) {
-        USERS = USERS.filter(user=>{
-            user.id!==id;
-        })
+        this.usersService.deleteUser(id);
         return "Deleted!"
     }
 }
